d464e3d055c36fdf47d4e2f2a1cb5ac7ea2f5076
Matthew Maloney <matthewgmaloney@gmail.com>
2024-01-04 14:56:10 -0600

    views updated for HHPNW

--- bangazonapi/views/item.py
+++ bangazonapi/views/item.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from bangazonapi.models import Item, User, Category
+from bangazonapi.models import Item, Category
 from .category import CategorySerializer
 
 class ItemView(ViewSet):
@@ -13,42 +13,30 @@ class ItemView(ViewSet):
     return Response(serializer.data, status=status.HTTP_200_OK)
   
   def list(self, request):
-    items = Item.objects.all()
-    
-    seller = request.query_params.get('sellerId', None)
-    
-    if seller is not None:
-      items = items.filter(seller_id = seller)
-    
+    items = Item.objects.all()    
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
   
   def create(self, request):
-    seller = User.objects.get(id=request.data['sellerId'])
     category = Category.objects.get(id=request.data['categoryId'])
     item = Item.objects.create(
       name = request.data['name'],
       price = request.data['price'],
-      available_quantity = request.data['availableQuantity'],
       image_url = request.data['imageUrl'],
       category = category,
-      seller = seller
     )
     serializer = ItemSerializer(item, many=False)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
   def update(self, request, pk):
-    seller = User.objects.get(id=request.data['sellerId'])
     category = Category.objects.get(id=request.data['categoryId'])
     item = Item.objects.get(pk=pk)
     
     item.name = request.data['name']
     item.price = request.data['price']
-    item.available_quantity = request.data['availableQuantity']
     item.image_url = request.data['imageUrl']
     item.category = category
-    item.seller = seller
-    
+
     item.save()
     
     serializer = ItemSerializer(item, many=False)
@@ -60,14 +48,8 @@ class ItemView(ViewSet):
     item.delete()
     return Response(None, status=status.HTTP_204_NO_CONTENT)
 
-class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ('id', 'uid', 'first_name', 'last_name', 'bio', 'email', 'address', 'phone_number', 'is_seller')
-
 class ItemSerializer(serializers.ModelSerializer):
   category = CategorySerializer(many=False)
-  seller = UserSerializer(many=False)
   class Meta:
     model = Item
-    fields = ('id', 'name', 'price', 'available_quantity', 'image_url', 'category', 'seller')
+    fields = ('id', 'name', 'price', 'image_url', 'category')
