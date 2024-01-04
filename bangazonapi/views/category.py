e834c0ba67afb5845b9041ed0f81755a0c2adb68
Matthew Maloney <matthewgmaloney@gmail.com>
2024-01-04 15:24:03 -0600

    updated order view for create and delete

--- bangazonapi/views/category.py
+++ bangazonapi/views/category.py
@@ -8,6 +8,7 @@ class CategoryView(ViewSet):
   """views for category"""
   def list(self, request):
     categories = Category.objects.all()
+        
     serializer = CategorySerializer(categories, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
